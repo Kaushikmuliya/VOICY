b71ef0bd7bc5a66b107c32034b38fe0212c0a6a6
Kaushikmuliya <kaushikmuliya82@gmail.com>
2025-05-31 12:27:34 +0530

    Contains listen command functionalities using speech_recognition library

---
+++ listen.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+def listen_command():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+        return command
+    except sr.UnknownValueError:
+        print("Sorry, I did not catch that.")
+        return None
+    except sr.RequestError:
+        print("Speech service Unavailable.")
+        return None
+    
+    #The function listens through your microphone,
+    #  tries to convert what you say into text using Google’s speech API,
+    #  and returns that text. If it can’t understand or there’s a problem,
+    #  it gracefully handles errors and returns nothing.
